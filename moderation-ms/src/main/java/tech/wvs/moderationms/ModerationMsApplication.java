5425c1d3656b0674d4d2c3effdc18248da8bfe5c
wallacevilela7 <wallace.vilela9@gmail.com>
2025-07-17 11:14:04 -0300

    Setup moderation service microservice

---
+++ moderation-ms/src/main/java/tech/wvs/moderationms/ModerationMsApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.moderationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModerationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModerationMsApplication.class, args);
+    }
+
+}
